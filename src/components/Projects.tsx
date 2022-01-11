8f22e3d564fdb6e3664593ed88aa01ee8a309fab
Marika <mashakonturova@gmail.com>
2022-01-11 15:47:11 +0300

    add links in project page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import travelly from "../assets/img/travelly.jpg";
 import elena from "../assets/img/elena-joy.jpg";
+import {NavLink} from 'react-router-dom'
 
 export function Projects() {
   return (
     <section className="projects">
       <div className="travelly">
         <h2>Travelly</h2>
-        <img src={travelly} alt="travelly-project" />
+      <a href="https://marikakonturova.github.io/Travel-website/"> <img src={travelly} alt="travelly-project" /></a> 
       </div>
       <div className="elena">
         <h2>Elena Joy</h2>
-        <img src={elena} alt="travelly-project" />
+        <a href='https://marikakonturova.github.io/photoraphy-website/'>
+        <img src={elena} alt="travelly-project" /></a>
       </div>
     </section>
   );
